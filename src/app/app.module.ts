601b1e3716c526b1d1fc630e85e9520771f878a7
Yassin Assim <yassinassim0@gmail.com>
2019-03-11 14:59:11 +0100

    feat: create all pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-import { MDBBootstrapModule } from 'angular-bootstrap-md'
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { NavbarComponent } from './commons/navbar/navbar.component';
+import { AboutComponent } from './pages/about/about.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { FooterComponent } from './commons/footer/footer.component';
+import { ProfilComponent } from './pages/profil/profil.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    AdminComponent,
+    HomeComponent,
+    CartComponent,
+    ContactComponent,
+    FooterComponent,
+    ProfilComponent,
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule
+    MDBBootstrapModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
