d3204dd8e208d640d8e5106438c9a09189cf33ac
Faresse TAHLAITI <faresse.tahlaiti@aol.com>
2019-03-12 17:02:10 +0100

    page contact et validators de tes morts

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,19 +1,41 @@
-import { Component, OnInit} from '@angular/core';
-
+import { Component, HostListener } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ContactService } from '../../services/contact.service';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
 
+  contactForm: FormGroup;
+  disabledSubmitButton: boolean = true;
   
+  @HostListener('input') oninput() {
+    if (this.contactForm.valid) {
+      this.disabledSubmitButton = false;
+      }
+    }
+
+  constructor(private fb: FormBuilder, private contactService: ContactService) {
+
+    this.contactForm = this.fb.group({
+      'contactFormEmail': ['', Validators.compose([Validators.required, Validators.email])],
+      'contactFormLastname': ['', Validators.required],
+      'contactFormFirstname': ['', Validators.required],
+      'contactFormMessage': ['', Validators.required]
+      });
+    }
   
-
-  ngOnInit() {
-  }
-
-  
-
+    onSubmit() {
+      // this.contactService.sendMessage(this.contactForm.value).subscribe(() => {
+      //   alert('Your message has been sent.');
+      //   this.contactForm.reset();
+      //   this.disabledSubmitButton = true;
+      // }, error => {
+      //   console.log('Error', error);
+      // });
+      this.contactService.sendMessage2(this.contactForm.value);
+    }
 }
