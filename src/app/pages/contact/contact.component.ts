13853ff0f971cad9a47e2f5eaef617782d90b924
Faresse TAHLAITI <faresse.tahlaiti@aol.com>
2019-03-12 12:27:03 +0100

    contact

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, HostListener } from '@angular/core';
-import { ConnextionService } from '../contact/connextion.service';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-contact',
@@ -9,39 +8,12 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class ContactComponent implements OnInit {
 
-  contactForm: FormGroup;
-  disabledSubmitButton: boolean = true;
-  optionsSelect: Array<any>;
-
-  @HostListener('input') oninput() {
-
-  if (this.contactForm.valid) {
-    this.disabledSubmitButton = false;
-    }
+  
   }
 
   ngOnInit() {
   }
 
-  constructor(private fb: FormBuilder, private connectionService: ConnextionService) {
-
-    this.contactForm = fb.group({
-      'contactFormName': ['', Validators.required],
-      'contactFormEmail': ['', Validators.compose([Validators.required, Validators.email])],
-      'contactFormSubjects': ['', Validators.required],
-      'contactFormMessage': ['', Validators.required],
-      'contactFormCopy': [''],
-      });
-    }
-
-  onSubmit() {
-    this.connectionService.sendMessage(this.contactForm.value).subscribe(() => {
-      alert('Your message has been sent.');
-      this.contactForm.reset();
-      this.disabledSubmitButton = true;
-    }, error => {
-      console.log('Error', error);
-    });
-  }
+  
 
 }
