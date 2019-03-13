59644e8eb61353992da9f6a171bfdfa0a373fd17
Julien Laforge <julien.laforge@gmx.fr>
2019-03-13 11:32:14 +0100

    contact form done + some changes on the navbar & the footer

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,5 +1,7 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {Router} from '@angular/router';
+
 import { ContactService } from '../../services/contact.service';
 
 @Component({
@@ -7,35 +9,43 @@ import { ContactService } from '../../services/contact.service';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
 
-  contactForm: FormGroup;
-  disabledSubmitButton: boolean = true;
-  
-  @HostListener('input') oninput() {
-    if (this.contactForm.valid) {
-      this.disabledSubmitButton = false;
-      }
+    contactForm: FormGroup;
+    submitted = false;
+
+    constructor(private formBuilder: FormBuilder,  private contactService: ContactService, private router: Router) { }
+
+
+    ngOnInit() {
+        this.contactForm = this.formBuilder.group({
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            email: ['', Validators.compose([Validators.required, Validators.email])],
+            message: ['', Validators.required],
+        });
     }
 
-  constructor(private fb: FormBuilder, private contactService: ContactService) {
+    // convenience getter for easy access to form fields
+    get f() { return this.contactForm.controls; }
 
-    this.contactForm = this.fb.group({
-      'contactFormEmail': ['', Validators.compose([Validators.required, Validators.email])],
-      'contactFormLastname': ['', Validators.required],
-      'contactFormFirstname': ['', Validators.required],
-      'contactFormMessage': ['', Validators.required]
-      });
-    }
-  
     onSubmit() {
-      // this.contactService.sendMessage(this.contactForm.value).subscribe(() => {
-      //   alert('Your message has been sent.');
-      //   this.contactForm.reset();
-      //   this.disabledSubmitButton = true;
-      // }, error => {
-      //   console.log('Error', error);
-      // });
-      this.contactService.sendMessage2(this.contactForm.value);
+        this.submitted = true;
+        // stop here if form is invalid
+        if (this.contactForm.invalid) {
+            return;
+        }
+
+        alert('Merci ' + this.contactForm.value.firstName + ', votre message a bien été envoyé');
+        this.router.navigateByUrl('/');
+
+        // this.contactService.sendMessage(this.contactForm.value).subscribe(() => {
+        //   alert('Your message has been sent.');
+        //   this.contactForm.reset();
+        //   this.disabledSubmitButton = true;
+        // }, error => {
+        //   console.log('Error', error);
+        // });
+        this.contactService.sendMessage2(this.contactForm.value);
     }
 }
