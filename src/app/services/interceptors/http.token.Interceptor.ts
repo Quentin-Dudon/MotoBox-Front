c455d948673ecd7534e91b00a8ca6708de0bdc93
Julien Laforge <julien.laforge@gmx.fr>
2019-03-19 15:08:35 +0100

    l'interceptor ajoute le token jwt motobox aux requêtes s'il existe

--- src/app/services/interceptors/http.token.Interceptor.ts
+++ src/app/services/interceptors/http.token.Interceptor.ts
@@ -1,24 +1,32 @@
 import { Injectable } from '@angular/core';
 import {
-  HttpInterceptor,
-  HttpRequest,
-  HttpHandler,
-  HttpEvent
+    HttpInterceptor,
+    HttpRequest,
+    HttpHandler,
+    HttpEvent
 } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
 
+import { AuthService } from './../auth.service';
+
 @Injectable()
 export class HttpTokenInterceptor implements HttpInterceptor {
+
+  constructor(private auth: AuthService) {}
+
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-      // add a custom header
-    const customReq = request.clone({
-        headers: request.headers.set('app-language', 'it')
-      });
-  
+      // add a custom header to ALL requests
+      if (this.auth.getToken() !== undefined) {
+        request = request.clone({   
+            setHeaders: {
+                Authorization: `${this.auth.getToken}`
+            }
+        });
+    }
       // pass on the modified request object
     return next.handle(request);
   }
