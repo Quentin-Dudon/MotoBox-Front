e59e12df019080a443aec2f0a144745c5a89aeba
Takeshi69 <qdudon@gmail.com>
2019-03-19 14:03:06 +0100

    refacto

--- src/app/pages/home/results/response/response.component.ts
+++ src/app/pages/home/results/response/response.component.ts
@@ -1,10 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import data from '../../../../../assets/mocks/ads.json';
-import { AdsService } from '../../../../services/ads/ads.service';
-import { NotFoundError } from '../../../../shared/error/not-found-error';
-import { AccessDeniedError } from '../../../../shared/error/access-denied-error';
-import { ServerError } from '../../../../shared/error/server-error';
-import { AppError } from '../../../../shared/error/app-error';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-response',
@@ -13,37 +7,11 @@ import { AppError } from '../../../../shared/error/app-error';
 })
 export class ResponseComponent implements OnInit {
 
-  ads: any;
+  @Input() ads;
 
-  constructor(private adsService: AdsService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.ads = [];
-    this.adsService.getAds()
-      .subscribe(
-        response => {
-          this.ads = response;
-          console.log('ADS :::', this.ads);
-        },
-        (error: AppError) => {
-          this.handleError(error);
-        });
   }
 
-  // -------------- ERRORS -------------- //
-  private handleError(error) {
-    let message = '';
-    console.log('ERROR ::', error);
-    console.log('Status ::', error.status);
-
-    if (error instanceof NotFoundError) { // utilisateur non trouvée ;
-      message = 'Le produit demandé n\'existe pas.';
-    } else if (error.status instanceof AccessDeniedError) { // acces refusé
-      message = 'Acces refusé.';
-    } else if (error.status instanceof ServerError) { // serveur KO
-      message = 'le serveur n\'a pas répondu, veuillez réessayer ulterieurement.';
-    } else { // others
-      message = 'Une erreur inattendue s\'est produite, veuillez réessayer de vous connecter.';
-    }
-  }
 }
